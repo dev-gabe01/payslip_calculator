1b5a55807d4540ac6f173a7d3a6966ed35e714e1
Gabe <gabriel.nagassa>
2021-11-27 00:14:06 +1100

    added tax module

--- GenerateMonthlyPayslip.py
+++ GenerateMonthlyPayslip.py
@@ -7,15 +7,9 @@ import os
 import sys
 import getopt
 import re
+import tax_module
 
-def help():
-	print("Usage: \n")
-	print("-n,  --name  : Set name\n")
-	print("-s,  --salary   : Set yearly salary \n")
-	print("Example: python GenerateMonthlyPayslip.py -n Bernard Black -s 21000")
-	sys.exit(2)
 
-#opts, args = getopt.getopt(sys.argv[1:],"nsh:",["name","salary","help"])
 opts, args = getopt.getopt(sys.argv[1:],"n:s:h",["name=","salary=", "help"])
 option_dict = {
 		"-n": "name",
@@ -28,7 +22,7 @@ foundSalary = False
 
 for opt, arg in opts:
 	if opt in ("-h", "--help"):
-		help()
+		tax_module.help()
 	if opt in ("-n", "--name"):
 		foundName = True
 		name = arg
@@ -44,50 +38,8 @@ if not foundSalary:
     print("A salary is required")
     help()
 
-def monthly_income(salary):
-	y_income = int(salary)
-	m_income = y_income/12
-	return m_income
-
-def tax_on_income(salary):
-	y_income = int(salary)
-	y_taxed = 0
-	m_taxed = 0
-	if y_income < 20001:
-		pass
-	elif y_income > 20001 and y_income < 40000:
-		y_taxed = (y_income - 20000) * 0.1
-	elif y_income > 40001 and y_income < 80000:
-		y_taxed = ((40000 - 20000) * 0.1 ) + ((y_income - 40000) * 0.2)
-	elif y_income > 80001 and y_income < 180000:
-		y_taxed = ((40000 - 20000) * 0.1 ) + ((80000 - 40000) * 0.2) + ((y_income - 80000) * 0.3)
-	elif y_income > 180001:
-		y_taxed = ((40000 - 20000) * 0.1 ) + ((80000 - 40000) * 0.2) + ((180000 - 80000) * 0.3) + ((y_income - 180000) * 0.4)
-	m_taxed = y_taxed/12
-	return y_taxed,m_taxed
-
-def net_income(y_income,y_taxed,m_income,m_taxed):
-	y_net_income = 0
-	m_net_income = 0
-	y_net_income = int(y_income) - int(y_taxed)
-	m_net_income = int(m_income) - int(m_taxed)
-	return y_net_income,m_net_income
-
-
-def main(name,salary):
-	#WRITE A SUMMARY THING HERE BEOFRE WRITE TO FILE
-	y_taxed,m_taxed = tax_on_income(salary)
-	y_net,m_net = net_income(salary,y_taxed,monthly_income(salary),m_taxed)
-	print("\nSummary")
-	print("-----------------")
-	print("The Monthly Payslip for: %s" % name)
-	print("Gross Yearly Income: %s" % salary)
-	print("Net Yearly Income: %s" % y_net)
-	print("Gross Monthly Income: %s" % monthly_income(salary))
-	print("Net Monthly Income: %s" % m_net)
-
 
 if __name__ == "__main__":
 	if len(sys.argv) <= 3:
-		help()
-	main(name,salary)
+		tax_module.help()
+	tax_module.main(name,salary)
